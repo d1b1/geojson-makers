f58a157596173a5a431a0c5801ccfeb2ae1ed86f
Stephan L. Smith <Stephan.Smith.BC93@gmail.com>
2014-02-11 22:52:55 -0500

    Updated the index.js.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var request = require('request')
     , _     = require('underscore')
     , fs    = require('fs')
-var argv    = require('optimist').argv
-var querystring = require('querystring')
+    , argv  = require('optimist').argv
+    , querystring = require('querystring')
+    , makers = []
+    , params = { size: 20000 }
 
-var makers = [];
-var params = { size: 20000 }
 if (argv.country) params.country = argv.country
 var filename = (argv.country || 'all') + '.geojson'
 
@@ -13,10 +13,9 @@ request.get({ url: 'http://api.formagg.io/maker/search?' + querystring.stringify
 
   _.each(data.body.results, function(maker) {
   	if (maker.location.lat && maker.location.lng) {
-
   	  var img = maker.logourl ? '<img src="' + (maker.logourl || '') + '"><br>' : ''
 
-  	  var d = { 
+  	  var data = { 
   	  	"type": "Feature", 
   	    "id": maker._id.toString(), 
   	    "properties": {
@@ -25,7 +24,7 @@ request.get({ url: 'http://api.formagg.io/maker/search?' + querystring.stringify
   	    	"stroke-width": 1,
   	    	"Maker": maker.name || 'NA',
   	    	"Where": (maker.country || '') + ' ' + (maker.state || ''),
-  	    	"Summary": img + '<a href="http://api.formagg.io/maker/' + maker._id + '">More</a>',
+  	    	"Summary": img + '<a href="http://api.formagg.io/maker/' + maker._id + '">Raw JSON</a>',
   	    }, 
   	    "geometry": { 
   	    	"type": "Point", 
@@ -33,20 +32,18 @@ request.get({ url: 'http://api.formagg.io/maker/search?' + querystring.stringify
   	    } 
   	  }
 
-  	  makers.push(d)
+  	  makers.push(data)
   	}
   })
 
   var geojson = { "type": "FeatureCollection", "features": makers }
 
-      fs.writeFile('./' + filename, JSON.stringify(geojson, null, 4), function(err) {
-	    if(err) {
-	        console.log(err);
-	    } else {
-	        console.log("The file was saved!");
-	    }
-	  })
-
-  //console.log(geojson)
+  fs.writeFile('./' + filename, JSON.stringify(geojson, null, 4), function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("The file was saved!");
+    }
+  })
 })
 
