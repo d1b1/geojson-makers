ff0b7dc92ec50a4b5da17950662a8b60c060978d
Stephan L. Smith <Stephan.Smith.BC93@gmail.com>
2014-02-11 19:24:34 -0500

    Added an initial dump.

---
+++ index.js
@@ -0,0 +1,40 @@
+var request = require('request')
+var _ = require('underscore')
+var fs = require('fs')
+
+var makers = [];
+request.get({ url: 'http://api.formagg.io/maker/search?name=b&size=10', json: true }, function(err, data) {
+
+  _.each(data.body.results, function(maker) {
+  	if (maker.location) {
+  	  var d = { 
+  	  	"type": "Feature", 
+  	    "id": maker._id.toString(), 
+  	    "properties": { 
+  	    	"Name": maker.name || 'NA', 
+  	    	"Country": maker.country || '',
+  	    	"State": maker.state | '' 
+  	    }, 
+  	    "geometry": { 
+  	    	"type": "Point", 
+  	    	"coordinates": [ maker.location.lng, maker.location.lat ] 
+  	    } 
+  	  }
+
+  	  makers.push(d)
+  	}
+  })
+
+  var geojson = { "type": "FeatureCollection", "features": makers }
+
+      fs.writeFile("./test.geojson", JSON.stringify(geojson), function(err) {
+	    if(err) {
+	        console.log(err);
+	    } else {
+	        console.log("The file was saved!");
+	    }
+	  })
+
+  console.log(geojson)
+})
+
